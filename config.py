f05e048a2dba93bda3da2b75e95b178b1b3a3e30
phil-7 <philippealec9@gmail.com>
2026-06-10 18:00:36 -0400

    Updated config

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
 # config.py      - Stores application settings such as host, port, data folder path, and language defaults
+
+DATA_FOLDER = "data/"
+FLASK_PORT= 5000
+FLASK_HOST = "0.0.0.0"
+LANGUAGES = {
+    "en" : "English",
+    "ht" : "Haitian Creole",
+    "fr" : "French"
+}
+DATABASE_PATH = "library.db"
