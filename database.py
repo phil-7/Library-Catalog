a1c0854d468a5fd3cbb694ec79b556ad1cf449fd
phil-7 <philippealec9@gmail.com>
2026-06-10 18:34:26 -0400

    Created database.py and library.db

--- database.py
+++ database.py
@@ -1,1 +1,21 @@
 # database.py    - Manages SQLite connection, schema, and queries for ZIM file metadata
+import config
+import sqlite3
+
+# Create a database if it does not exist yet
+def init_db():
+    connection = sqlite3.connect(config.DATABASE_PATH)
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS library(" \
+    "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+    "filename TEXT," \
+    "title TEXT," \
+    "language TEXT," \
+    "path TEXT" \
+    ")")
+    connection.commit()
+    connection.close()
+
+if __name__ == "__main__":
+    init_db()
+    print("Database initialized!")
